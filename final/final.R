13f76e00be03f642de9ce144a72ac44b12472118
Utku Toraman <utkutoraman@gmail.com>
2020-01-02 20:46:12 +0300

    changes to final project

--- final/final.R
+++ final/final.R
@@ -9,7 +9,7 @@ library(mlbench)
 
 set.seed(123)
 
-setwd("/home/utku/ceng474/final")
+setwd("/home/utku/ceng474/datascience/final")
 df<-read.csv('data/pulsar_stars.csv')
 
 str(df)
@@ -20,7 +20,7 @@ df$target_class <- as.factor(df$target_class)
 
 # dropping highly correlated features (p > 0.9)
 df_cols <- colnames(df)
-rejected_cols <- c('Skewness.of.the.DM.SNR.curve', 'Skewness.of.the.integrated.profile')
+  rejected_cols <- c('Skewness.of.the.DM.SNR.curve', 'Skewness.of.the.integrated.profile')
 df_cols %in% rejected_cols
 df_cols<-df_cols[! df_cols %in% rejected_cols]
 
@@ -44,7 +44,8 @@ fitControl <- trainControl(method = "repeatedcv", number = 10, repeats=5, savePr
 
 # rpart
 model1 <- train(target_class ~ ., data = training,
-                method = "rpart",
+                method = "rpart", 
+                preProcess=c("scale","center"),
                 trControl = fitControl,
                 na.action = na.omit)
 model1
@@ -52,24 +53,20 @@ pred1 <- predict(model1, testing[, 1:6])
 confusionMatrix(data = pred1, reference = testing$target_class)
 
 pred1_probs <- predict(model1, testing[, 1:6], type = "prob")
-roc1 <- multiclass.roc(testing$target_class, pred1_probs, plot=TRUE, 
-                       auc.polygon=TRUE, max.auc.polygon=TRUE, grid=TRUE, 
-                       print.auc=TRUE)
 
-par(mfrow=c(1,1))
-roc1 <- roc(testing$target_class, pred1_probs$X0, smoothed = TRUE, 
-                 ci=TRUE, ci.alpha=0.9, stratified=FALSE, plot=TRUE, 
-                 auc.polygon=TRUE, max.auc.polygon=TRUE, grid=TRUE, 
-                 print.auc=TRUE, show.thres=TRUE)
-sens.ci1 <- ci.se(roc1)
-plot(sens.ci1, type="shape", col="lightblue")
-plot(sens.ci1, type="bars")
+dev.off()
+
+roc1 <- roc(testing$target_class, pred1_probs$X1, 
+            ci=TRUE,plot=TRUE, auc.polygon=TRUE, 
+            max.auc.polygon=TRUE, grid=TRUE,
+            print.auc=TRUE)
 
 roc1[["auc"]]
 
 # knn
 model2 <- train(target_class ~ ., data = training, 
-                method = "knn",
+                method = "knn", 
+                preProcess=c("scale","center"),
                 trControl= fitControl,
                 tuneLength = 10,
                 na.action = na.omit)
@@ -79,11 +76,42 @@ confusionMatrix(data = pred2, reference = testing$target_class)
 
 pred2_probs <- predict(model2, testing[, 1:6], type = "prob")
 dev.off()
-roc2 <- roc(testing$target_class, pred2_probs$X0, smoothed = TRUE, 
-            ci=TRUE, ci.alpha=0.9, stratified=FALSE, plot=TRUE, 
-            auc.polygon=TRUE, max.auc.polygon=TRUE, grid=TRUE, 
-            print.auc=TRUE, show.thres=TRUE)
-sens.ci2 <- ci.se(roc2)
-plot(sens.ci2, type="shape", col="lightblue")
-plot(sens.ci2, type="bars")
+roc2 <- roc(testing$target_class, pred2_probs$X1,
+            ci=TRUE,plot=TRUE, auc.polygon=TRUE, 
+            max.auc.polygon=TRUE, grid=TRUE,
+            print.auc=TRUE)
 roc2[["auc"]]
+# C5.0
+model3 <- train(target_class ~ ., data = training, 
+                method = "C5.0", 
+                preProcess=c("scale","center"),
+                trControl= fitControl,
+                na.action = na.omit)
+model3
+pred3 <- predict(model3, testing[, 1:6])
+confusionMatrix(data = pred3, reference = testing$target_class)
+
+pred3_probs <- predict(model3, testing[, 1:6], type = "prob")
+dev.off()
+roc3 <- roc(testing$target_class, pred3_probs$X1,
+            ci=TRUE,plot=TRUE, auc.polygon=TRUE, 
+            max.auc.polygon=TRUE, grid=TRUE,
+            print.auc=TRUE)
+roc3[["auc"]]
+# Logistic Regression
+model4 <- train(target_class ~ ., data = training, 
+                method = "vglmAdjCat", 
+                preProcess=c("scale","center"),
+                trControl= fitControl,
+                na.action = na.omit)
+model4
+pred4 <- predict(model4, testing[, 1:6])
+confusionMatrix(data = pred4, reference = testing$target_class)
+
+pred4_probs <- predict(model4, testing[, 1:6], type = "prob")
+dev.off()
+roc4 <- roc(testing$target_class, pred4_probs$X1,
+            ci=TRUE,plot=TRUE, auc.polygon=TRUE, 
+            max.auc.polygon=TRUE, grid=TRUE,
+            print.auc=TRUE)
+roc4[["auc"]]
